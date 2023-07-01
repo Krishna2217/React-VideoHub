a5161e5727263edc333a35791a5f1a89a06616fa
Krishna2217 <rautkrishna2217@gmail.com>
2023-07-01 17:19:03 +0545

    upload completed

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import { Container } from '@chakra-ui/react'
+import React from 'react'
+
+const Login = () => {
+    return (
+        <Container maxW={'container.xl'} h={'100vh'} p={'16'}>
+            <form>
+                
+            </form>
+        </Container>
+    )
+}
+
+export default Login
